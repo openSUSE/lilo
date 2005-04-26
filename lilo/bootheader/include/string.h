529267532927e1e322e8de6bb44621f357163b86
Olaf Hering <olh@suse.de>
2005-04-26 12:31:49 +0000

    add private types.h and string.h

---
+++ lilo/bootheader/include/string.h
@@ -0,0 +1,6 @@
+#ifndef __STRING_H__
+#define __STRING_H__
+
+extern size_t strlen(const char *);
+
+#endif /* __STRING_H__ */
---
+++ lilo/bootheader/include/types.h
@@ -0,0 +1,7 @@
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+#include <ppc32-types.h>
+#include <stddef.h>
+
+#endif /* __TYPES_H__ */
