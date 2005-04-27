493c7bb16b368de4ce172632220a3d70a5bfff3f
Olaf Hering <olh@suse.de>
2005-04-27 12:48:29 +0000

    fix compile error lib/strstr.c: In function `strstr': lib/strstr.c:79: warning: deprecated use of label at end of compound statement

--- yaboot/lib/strstr.c
+++ yaboot/lib/strstr.c
@@ -76,7 +76,9 @@ strstr (phaystack, pneedle)
 	      a = *++haystack;
 	      if (a == '\0')
 		goto ret0;
-shloop:	    }
+shloop:
+	    ;
+	    }
           while (a != b);
 
 jin:	  a = *++haystack;
