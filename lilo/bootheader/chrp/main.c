e2b91f0becd32cc3f92810aa727e6ffb3614ee75
Olaf Hering <olh@suse.de>
2005-05-30 17:06:09 +0000

    use new zlib

--- lilo/bootheader/chrp/main.c
+++ lilo/bootheader/chrp/main.c
@@ -16,7 +16,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <prom.h>
-#include "zlib.h"
+#include <zlib/zutil.h>
 
 static void do_gunzip(void *, int, unsigned char *, int *);
 extern void flush_cache(void *, unsigned long);
@@ -280,12 +280,11 @@ struct memchunk {
 
 static struct memchunk *freechunks;
 
-void *zalloc(void *x, unsigned items, unsigned size)
+static void *zalloc(unsigned int size)
 {
 	void *p;
 	struct memchunk **mpp, *mp;
 
-	size *= items;
 	size = _ALIGN(size, sizeof(struct memchunk));
 	heap_use += size;
 	if (heap_use > heap_max)
@@ -307,28 +306,11 @@ void *zalloc(void *x, unsigned items, unsigned size)
 	return p;
 }
 
-void zfree(void *x, void *addr, unsigned nb)
-{
-	struct memchunk *mp = addr;
-
-	nb = _ALIGN(nb, sizeof(struct memchunk));
-	heap_use -= nb;
-	if (avail_ram == addr + nb) {
-		avail_ram = addr;
-		return;
-	}
-	mp->size = nb;
-	mp->next = freechunks;
-	freechunks = mp;
-}
-
 #define HEAD_CRC	2
 #define EXTRA_FIELD	4
 #define ORIG_NAME	8
 #define COMMENT		0x10
 #define RESERVED	0xe0
-
-#define DEFLATED	8
 
 static void do_gunzip(void *dst, int dstlen, unsigned char *src, int *lenp)
 {
@@ -338,7 +320,7 @@ static void do_gunzip(void *dst, int dstlen, unsigned char *src, int *lenp)
 	/* skip header */
 	i = 10;
 	flags = src[3];
-	if (src[2] != DEFLATED || (flags & RESERVED) != 0) {
+	if (src[2] != Z_DEFLATED || (flags & RESERVED) != 0) {
 		printf("bad gzipped data\n\r");
 		exit();
 	}
@@ -357,9 +339,9 @@ static void do_gunzip(void *dst, int dstlen, unsigned char *src, int *lenp)
 		exit();
 	}
 
-	s.zalloc = zalloc;
-	s.zfree = zfree;
-	r = inflateInit2(&s, -MAX_WBITS);
+	memset(&s, 0, sizeof(s));
+	s.workspace = zalloc(zlib_inflate_workspacesize());
+	r = zlib_inflateInit2(&s, -MAX_WBITS);
 	if (r != Z_OK) {
 		printf("inflateInit2 returned %d\n\r", r);
 		exit();
@@ -368,12 +350,12 @@ static void do_gunzip(void *dst, int dstlen, unsigned char *src, int *lenp)
 	s.avail_in = *lenp - i;
 	s.next_out = dst;
 	s.avail_out = dstlen;
-	r = inflate(&s, Z_FINISH);
+	r = zlib_inflate(&s, Z_FINISH);
 	if (r != Z_OK && r != Z_STREAM_END) {
 		printf("inflate returned %d msg: %s\n\r", r, s.msg);
 		exit();
 	}
 	*lenp = s.next_out - (unsigned char *) dst;
-	inflateEnd(&s);
+	zlib_inflateEnd(&s);
 }
 
