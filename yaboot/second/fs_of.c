23593dc4012a969551809d1051792bf675d26957
Olaf Hering <olh@suse.de>
2005-12-03 10:01:02 +0000

    +- remove leading zeros from partition number in of_open in yaboot. +  OpenFirmware handles it as whole disk (136537 - LTC20058)

--- yaboot/second/fs_of.c
+++ yaboot/second/fs_of.c
@@ -92,13 +92,13 @@ of_open(struct boot_file_t* file, const char* dev_name,
      strncpy(buffer, dev_name, 768);
      strcat(buffer, ":");
      if (part) {
-	  char pn[3];
+	  char pn[12];
 	  if (part->sys_ind == LINUX_RAID) {
 		  DEBUG_F("skipping because partition is marked LINUX_RAID\n");
 		  DEBUG_LEAVE(FILE_ERR_BAD_FSYS);
 		  return FILE_ERR_BAD_FSYS;
 	  }
-	  sprintf(pn, "%02d", part->part_number);
+	  sprintf(pn, "%d", part->part_number);
 	  strcat(buffer, pn);
      }
      if (file_name && strlen(file_name)) {
