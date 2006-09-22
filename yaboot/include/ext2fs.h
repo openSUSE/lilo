b7e504f70af82ea8cd5ba13ea961c9a4b35ed1c9
Olaf Hering <olh@suse.de>
2006-09-22 20:10:29 +0000

    indent ext2fs.h

--- yaboot/include/ext2fs.h
+++ yaboot/include/ext2fs.h
@@ -67,31 +67,31 @@ typedef struct ext2_struct_u32_list *ext2_badblocks_list;
 typedef struct ext2_struct_dblist *ext2_dblist;
 
 struct ext2_super_block {
-	__u32	s_inodes_count;		/* Inodes count */
-	__u32	s_blocks_count;		/* Blocks count */
-	__u32	s_r_blocks_count;	/* Reserved blocks count */
-	__u32	s_free_blocks_count;	/* Free blocks count */
-	__u32	s_free_inodes_count;	/* Free inodes count */
-	__u32	s_first_data_block;	/* First Data Block */
-	__u32	s_log_block_size;	/* Block size */
-	__s32	s_log_frag_size;	/* Fragment size */
-	__u32	s_blocks_per_group;	/* # Blocks per group */
-	__u32	s_frags_per_group;	/* # Fragments per group */
-	__u32	s_inodes_per_group;	/* # Inodes per group */
-	__u32	s_mtime;		/* Mount time */
-	__u32	s_wtime;		/* Write time */
-	__u16	s_mnt_count;		/* Mount count */
-	__s16	s_max_mnt_count;	/* Maximal mount count */
-	__u16	s_magic;		/* Magic signature */
-	__u16	s_state;		/* File system state */
-	__u16	s_errors;		/* Behaviour when detecting errors */
-	__u16	s_minor_rev_level;	/* minor revision level */
-	__u32	s_lastcheck;		/* time of last check */
-	__u32	s_checkinterval;	/* max. time between checks */
-	__u32	s_creator_os;		/* OS */
-	__u32	s_rev_level;		/* Revision level */
-	__u16	s_def_resuid;		/* Default uid for reserved blocks */
-	__u16	s_def_resgid;		/* Default gid for reserved blocks */
+	__u32 s_inodes_count;	/* Inodes count */
+	__u32 s_blocks_count;	/* Blocks count */
+	__u32 s_r_blocks_count;	/* Reserved blocks count */
+	__u32 s_free_blocks_count;	/* Free blocks count */
+	__u32 s_free_inodes_count;	/* Free inodes count */
+	__u32 s_first_data_block;	/* First Data Block */
+	__u32 s_log_block_size;	/* Block size */
+	__s32 s_log_frag_size;	/* Fragment size */
+	__u32 s_blocks_per_group;	/* # Blocks per group */
+	__u32 s_frags_per_group;	/* # Fragments per group */
+	__u32 s_inodes_per_group;	/* # Inodes per group */
+	__u32 s_mtime;		/* Mount time */
+	__u32 s_wtime;		/* Write time */
+	__u16 s_mnt_count;	/* Mount count */
+	__s16 s_max_mnt_count;	/* Maximal mount count */
+	__u16 s_magic;		/* Magic signature */
+	__u16 s_state;		/* File system state */
+	__u16 s_errors;		/* Behaviour when detecting errors */
+	__u16 s_minor_rev_level;	/* minor revision level */
+	__u32 s_lastcheck;	/* time of last check */
+	__u32 s_checkinterval;	/* max. time between checks */
+	__u32 s_creator_os;	/* OS */
+	__u32 s_rev_level;	/* Revision level */
+	__u16 s_def_resuid;	/* Default uid for reserved blocks */
+	__u16 s_def_resgid;	/* Default gid for reserved blocks */
 	/*
 	 * These fields are for EXT2_DYNAMIC_REV superblocks only.
 	 *
@@ -105,212 +105,186 @@ struct ext2_super_block {
 	 * feature set, it must abort and not try to meddle with
 	 * things it doesn't understand...
 	 */
-	__u32	s_first_ino;		/* First non-reserved inode */
-	__u16   s_inode_size;		/* size of inode structure */
-	__u16	s_block_group_nr;	/* block group # of this superblock */
-	__u32	s_feature_compat;	/* compatible feature set */
-	__u32	s_feature_incompat;	/* incompatible feature set */
-	__u32	s_feature_ro_compat;	/* readonly-compatible feature set */
-	__u8	s_uuid[16];		/* 128-bit uuid for volume */
-	char	s_volume_name[16];	/* volume name */
-	char	s_last_mounted[64];	/* directory where last mounted */
-	__u32	s_algorithm_usage_bitmap; /* For compression */
+	__u32 s_first_ino;	/* First non-reserved inode */
+	__u16 s_inode_size;	/* size of inode structure */
+	__u16 s_block_group_nr;	/* block group # of this superblock */
+	__u32 s_feature_compat;	/* compatible feature set */
+	__u32 s_feature_incompat;	/* incompatible feature set */
+	__u32 s_feature_ro_compat;	/* readonly-compatible feature set */
+	__u8 s_uuid[16];	/* 128-bit uuid for volume */
+	char s_volume_name[16];	/* volume name */
+	char s_last_mounted[64];	/* directory where last mounted */
+	__u32 s_algorithm_usage_bitmap;	/* For compression */
 	/*
 	 * Performance hints.  Directory preallocation should only
 	 * happen if the EXT2_FEATURE_COMPAT_DIR_PREALLOC flag is on.
 	 */
-	__u8	s_prealloc_blocks;	/* Nr of blocks to try to preallocate*/
-	__u8	s_prealloc_dir_blocks;	/* Nr to preallocate for dirs */
-	__u16	s_reserved_gdt_blocks;	/* Per group table for online growth */
+	__u8 s_prealloc_blocks;	/* Nr of blocks to try to preallocate */
+	__u8 s_prealloc_dir_blocks;	/* Nr to preallocate for dirs */
+	__u16 s_reserved_gdt_blocks;	/* Per group table for online growth */
 	/*
 	 * Journaling support valid if EXT2_FEATURE_COMPAT_HAS_JOURNAL set.
 	 */
-	__u8	s_journal_uuid[16];	/* uuid of journal superblock */
-	__u32	s_journal_inum;		/* inode number of journal file */
-	__u32	s_journal_dev;		/* device number of journal file */
-	__u32	s_last_orphan;		/* start of list of inodes to delete */
-	__u32	s_hash_seed[4];		/* HTREE hash seed */
-	__u8	s_def_hash_version;	/* Default hash version to use */
-	__u8	s_jnl_backup_type; 	/* Default type of journal backup */
-	__u16	s_reserved_word_pad;
-	__u32	s_default_mount_opts;
-	__u32	s_first_meta_bg;	/* First metablock group */
-	__u32	s_mkfs_time;		/* When the filesystem was created */
-	__u32	s_jnl_blocks[17]; 	/* Backup of the journal inode */
-	__u32	s_reserved[172];	/* Padding to the end of the block */
+	__u8 s_journal_uuid[16];	/* uuid of journal superblock */
+	__u32 s_journal_inum;	/* inode number of journal file */
+	__u32 s_journal_dev;	/* device number of journal file */
+	__u32 s_last_orphan;	/* start of list of inodes to delete */
+	__u32 s_hash_seed[4];	/* HTREE hash seed */
+	__u8 s_def_hash_version;	/* Default hash version to use */
+	__u8 s_jnl_backup_type;	/* Default type of journal backup */
+	__u16 s_reserved_word_pad;
+	__u32 s_default_mount_opts;
+	__u32 s_first_meta_bg;	/* First metablock group */
+	__u32 s_mkfs_time;	/* When the filesystem was created */
+	__u32 s_jnl_blocks[17];	/* Backup of the journal inode */
+	__u32 s_reserved[172];	/* Padding to the end of the block */
 };
-struct ext2_group_desc
-{
-	__u32	bg_block_bitmap;		/* Blocks bitmap block */
-	__u32	bg_inode_bitmap;		/* Inodes bitmap block */
-	__u32	bg_inode_table;		/* Inodes table block */
-	__u16	bg_free_blocks_count;	/* Free blocks count */
-	__u16	bg_free_inodes_count;	/* Free inodes count */
-	__u16	bg_used_dirs_count;	/* Directories count */
-	__u16	bg_pad;
-	__u32	bg_reserved[3];
+struct ext2_group_desc {
+	__u32 bg_block_bitmap;	/* Blocks bitmap block */
+	__u32 bg_inode_bitmap;	/* Inodes bitmap block */
+	__u32 bg_inode_table;	/* Inodes table block */
+	__u16 bg_free_blocks_count;	/* Free blocks count */
+	__u16 bg_free_inodes_count;	/* Free inodes count */
+	__u16 bg_used_dirs_count;	/* Directories count */
+	__u16 bg_pad;
+	__u32 bg_reserved[3];
 };
 struct ext2_inode {
-	__u16	i_mode;		/* File mode */
-	__u16	i_uid;		/* Low 16 bits of Owner Uid */
-	__u32	i_size;		/* Size in bytes */
-	__u32	i_atime;	/* Access time */
-	__u32	i_ctime;	/* Creation time */
-	__u32	i_mtime;	/* Modification time */
-	__u32	i_dtime;	/* Deletion Time */
-	__u16	i_gid;		/* Low 16 bits of Group Id */
-	__u16	i_links_count;	/* Links count */
-	__u32	i_blocks;	/* Blocks count */
-	__u32	i_flags;	/* File flags */
+	__u16 i_mode;		/* File mode */
+	__u16 i_uid;		/* Low 16 bits of Owner Uid */
+	__u32 i_size;		/* Size in bytes */
+	__u32 i_atime;		/* Access time */
+	__u32 i_ctime;		/* Creation time */
+	__u32 i_mtime;		/* Modification time */
+	__u32 i_dtime;		/* Deletion Time */
+	__u16 i_gid;		/* Low 16 bits of Group Id */
+	__u16 i_links_count;	/* Links count */
+	__u32 i_blocks;		/* Blocks count */
+	__u32 i_flags;		/* File flags */
 	union {
 		struct {
-			__u32  l_i_reserved1;
+			__u32 l_i_reserved1;
 		} linux1;
 		struct {
-			__u32  h_i_translator;
+			__u32 h_i_translator;
 		} hurd1;
 		struct {
-			__u32  m_i_reserved1;
+			__u32 m_i_reserved1;
 		} masix1;
-	} osd1;				/* OS dependent 1 */
-	__u32	i_block[EXT2_N_BLOCKS];/* Pointers to blocks */
-	__u32	i_generation;	/* File version (for NFS) */
-	__u32	i_file_acl;	/* File ACL */
-	__u32	i_dir_acl;	/* Directory ACL */
-	__u32	i_faddr;	/* Fragment address */
+	} osd1;			/* OS dependent 1 */
+	__u32 i_block[EXT2_N_BLOCKS];	/* Pointers to blocks */
+	__u32 i_generation;	/* File version (for NFS) */
+	__u32 i_file_acl;	/* File ACL */
+	__u32 i_dir_acl;	/* Directory ACL */
+	__u32 i_faddr;		/* Fragment address */
 	union {
 		struct {
-			__u8	l_i_frag;	/* Fragment number */
-			__u8	l_i_fsize;	/* Fragment size */
-			__u16	i_pad1;
-			__u16	l_i_uid_high;	/* these 2 fields    */
-			__u16	l_i_gid_high;	/* were reserved2[0] */
-			__u32	l_i_reserved2;
+			__u8 l_i_frag;	/* Fragment number */
+			__u8 l_i_fsize;	/* Fragment size */
+			__u16 i_pad1;
+			__u16 l_i_uid_high;	/* these 2 fields    */
+			__u16 l_i_gid_high;	/* were reserved2[0] */
+			__u32 l_i_reserved2;
 		} linux2;
 		struct {
-			__u8	h_i_frag;	/* Fragment number */
-			__u8	h_i_fsize;	/* Fragment size */
-			__u16	h_i_mode_high;
-			__u16	h_i_uid_high;
-			__u16	h_i_gid_high;
-			__u32	h_i_author;
+			__u8 h_i_frag;	/* Fragment number */
+			__u8 h_i_fsize;	/* Fragment size */
+			__u16 h_i_mode_high;
+			__u16 h_i_uid_high;
+			__u16 h_i_gid_high;
+			__u32 h_i_author;
 		} hurd2;
 		struct {
-			__u8	m_i_frag;	/* Fragment number */
-			__u8	m_i_fsize;	/* Fragment size */
-			__u16	m_pad1;
-			__u32	m_i_reserved2[2];
+			__u8 m_i_frag;	/* Fragment number */
+			__u8 m_i_fsize;	/* Fragment size */
+			__u16 m_pad1;
+			__u32 m_i_reserved2[2];
 		} masix2;
-	} osd2;				/* OS dependent 2 */
+	} osd2;			/* OS dependent 2 */
 };
 struct struct_io_channel {
-	long	magic;
-	io_manager	manager;
-	char		*name;
-	int		block_size;
-	long	(*read_error)(io_channel channel,
-				      unsigned long block,
-				      int count,
-				      void *data,
-				      size_t size,
-				      int actual_bytes_read,
-				      long	error);
-	long	(*write_error)(io_channel channel,
-				       unsigned long block,
-				       int count,
-				       const void *data,
-				       size_t size,
-				       int actual_bytes_written,
-				       long error);
-	int		refcount;
-	int		flags;
-	int		reserved[14];
-	void		*private_data;
-	void		*app_data;
+	long magic;
+	io_manager manager;
+	char *name;
+	int block_size;
+	long (*read_error) (io_channel channel,
+			    unsigned long block, int count, void *data, size_t size, int actual_bytes_read, long error);
+	long (*write_error) (io_channel channel,
+			     unsigned long block, int count, const void *data, size_t size, int actual_bytes_written, long error);
+	int refcount;
+	int flags;
+	int reserved[14];
+	void *private_data;
+	void *app_data;
 };
 struct struct_io_manager {
 	long magic;
 	const char *name;
-	long (*open)(const char *name, int flags, io_channel *channel);
-	long (*close)(io_channel channel);
-	long (*set_blksize)(io_channel channel, int blksize);
-	long (*read_blk)(io_channel channel, unsigned long block,
-			      int count, void *data);
-	long (*write_blk)(io_channel channel, unsigned long block,
-			       int count, const void *data);
-	long (*flush)(io_channel channel);
-	long (*write_byte)(io_channel channel, unsigned long offset,
-				int count, const void *data);
-	long (*set_option)(io_channel channel, const char *option, 
-				const char *arg);
-	int		reserved[14];
+	long (*open) (const char *name, int flags, io_channel * channel);
+	long (*close) (io_channel channel);
+	long (*set_blksize) (io_channel channel, int blksize);
+	long (*read_blk) (io_channel channel, unsigned long block, int count, void *data);
+	long (*write_blk) (io_channel channel, unsigned long block, int count, const void *data);
+	long (*flush) (io_channel channel);
+	long (*write_byte) (io_channel channel, unsigned long offset, int count, const void *data);
+	long (*set_option) (io_channel channel, const char *option, const char *arg);
+	int reserved[14];
 };
 struct struct_ext2_filsys {
-	long			magic;
-	io_channel			io;
-	int				flags;
-	char *				device_name;
-	struct ext2_super_block	* 	super;
-	unsigned int			blocksize;
-	int				fragsize;
-	u32				group_desc_count;
-	unsigned long			desc_blocks;
-	struct ext2_group_desc *	group_desc;
-	int				inode_blocks_per_group;
-	ext2fs_inode_bitmap		inode_map;
-	ext2fs_block_bitmap		block_map;
-	long (*get_blocks)(ext2_filsys fs, u32 ino, u32 *blocks);
-	long (*check_directory)(ext2_filsys fs, u32 ino);
-	long (*write_bitmaps)(ext2_filsys fs);
-	long (*read_inode)(ext2_filsys fs, u32 ino,
-				struct ext2_inode *inode);
-	long (*write_inode)(ext2_filsys fs, u32 ino,
-				struct ext2_inode *inode);
-	ext2_badblocks_list		badblocks;
-	ext2_dblist			dblist;
-	__u32				stride;	/* for mke2fs */
-	struct ext2_super_block *	orig_super;
-	struct ext2_image_hdr *		image_header;
-	__u32				umask;
+	long magic;
+	io_channel io;
+	int flags;
+	char *device_name;
+	struct ext2_super_block *super;
+	unsigned int blocksize;
+	int fragsize;
+	u32 group_desc_count;
+	unsigned long desc_blocks;
+	struct ext2_group_desc *group_desc;
+	int inode_blocks_per_group;
+	ext2fs_inode_bitmap inode_map;
+	ext2fs_block_bitmap block_map;
+	long (*get_blocks) (ext2_filsys fs, u32 ino, u32 * blocks);
+	long (*check_directory) (ext2_filsys fs, u32 ino);
+	long (*write_bitmaps) (ext2_filsys fs);
+	long (*read_inode) (ext2_filsys fs, u32 ino, struct ext2_inode * inode);
+	long (*write_inode) (ext2_filsys fs, u32 ino, struct ext2_inode * inode);
+	ext2_badblocks_list badblocks;
+	ext2_dblist dblist;
+	__u32 stride;		/* for mke2fs */
+	struct ext2_super_block *orig_super;
+	struct ext2_image_hdr *image_header;
+	__u32 umask;
 	/*
 	 * Reserved for future expansion
 	 */
-	__u32				reserved[8];
+	__u32 reserved[8];
 
 	/*
 	 * Reserved for the use of the calling application.
 	 */
-	void *				priv_data;
+	void *priv_data;
 
 	/*
 	 * Inode cache
 	 */
-	struct ext2_inode_cache		*icache;
-	io_channel			image_io;
+	struct ext2_inode_cache *icache;
+	io_channel image_io;
 };
 struct ext2fs_struct_generic_bitmap {
-	long	magic;
-	ext2_filsys 	fs;
-	__u32		start, end;
-	__u32		real_end;
-	char	*	description;
-	char	*	bitmap;
-	long	base_error_code;
-	__u32		reserved[7];
+	long magic;
+	ext2_filsys fs;
+	__u32 start, end;
+	__u32 real_end;
+	char *description;
+	char *bitmap;
+	long base_error_code;
+	__u32 reserved[7];
 };
 #define io_channel_read_blk(c,b,n,d)	((c)->manager->read_blk((c),b,n,d))
-extern long ext2fs_block_iterate(ext2_filsys fs,
-				      u32	ino,
-				      int	flags,
-				      char *block_buf,
-				      int (*func)(ext2_filsys fs,
-						  u32	*blocknr,
-						  int	blockcnt,
-						  void	*priv_data),
-				      void *priv_data);
-extern long ext2fs_open(const char *name, int flags, int superblock,
-			     unsigned int block_size, io_manager manager,
-			     ext2_filsys *ret_fs);
+extern long ext2fs_block_iterate(ext2_filsys fs, u32 ino, int flags, char *block_buf,
+				 int (*func) (ext2_filsys fs, u32 * blocknr, int blockcnt, void *priv_data), void *priv_data);
+extern long ext2fs_open(const char *name, int flags, int superblock, unsigned int block_size, io_manager manager,
+			ext2_filsys * ret_fs);
 extern long ext2fs_close(ext2_filsys fs);
-long ext2fs_namei_follow(ext2_filsys fs, u32 root, u32 cwd,
-			      const char *name, u32 *inode);
+long ext2fs_namei_follow(ext2_filsys fs, u32 root, u32 cwd, const char *name, u32 * inode);
