551086af462c4e4f66fc761c651e7a11bfb42346
Olaf Hering <olh@suse.de>
2008-09-25 09:37:27 +0000

    Lindent utils/addnote.c

--- lilo/bootheader/utils/addnote.c
+++ lilo/bootheader/utils/addnote.c
@@ -35,12 +35,12 @@ char arch[] = "PowerPC";
 
 #define N_DESCR	6
 unsigned int descr[N_DESCR] = {
-     0xffffffff,		/* real-mode = true */
-     0x02000000,		/* real-base, i.e. where we expect OF to be */
-     0xffffffff,		/* real-size */
-     0xffffffff,		/* virt-base */
-     0xffffffff,		/* virt-size */
-     0x4000,			/* load-base */
+	0xffffffff,		/* real-mode = true */
+	0x02000000,		/* real-base, i.e. where we expect OF to be */
+	0xffffffff,		/* real-size */
+	0xffffffff,		/* virt-base */
+	0xffffffff,		/* virt-size */
+	0x4000,			/* load-base */
 };
 
 /* RPA note section */
@@ -54,14 +54,14 @@ char rpaname[] = "IBM,RPA-Client-Config";
  */
 #define N_RPA_DESCR	8
 unsigned int rpanote[N_RPA_DESCR] = {
-     0,				/* lparaffinity */
-     64,			/* min_rmo_size */
-     0,				/* min_rmo_percent */
-     40,			/* max_pft_size */
-     1,				/* splpar */
-     -1,			/* min_load */
-     0,				/* new_mem_def */
-     1,				/* ignore_my_client_config */
+	0,			/* lparaffinity */
+	64,			/* min_rmo_size */
+	0,			/* min_rmo_percent */
+	40,			/* max_pft_size */
+	1,			/* splpar */
+	-1,			/* min_load */
+	0,			/* new_mem_def */
+	1,			/* ignore_my_client_config */
 };
 
 #define ROUNDUP(len)	(((len) + 3) & ~3)
@@ -98,120 +98,115 @@ unsigned char buf[512];
 
 unsigned char elf_magic[4] = { 0x7f, 'E', 'L', 'F' };
 
-int
-main(int ac, char **av)
+int main(int ac, char **av)
 {
-     int fd, n, i;
-     int ph, ps, np;
-     int nnote, nnote2, ns;
+	int fd, n, i;
+	int ph, ps, np;
+	int nnote, nnote2, ns;
 
-     if (ac != 2) {
-	  fprintf(stderr, "Usage: %s elf-file\n", av[0]);
-	  exit(1);
-     }
-     fd = open(av[1], O_RDWR);
-     if (fd < 0) {
-	  perror(av[1]);
-	  exit(1);
-     }
+	if (ac != 2) {
+		fprintf(stderr, "Usage: %s elf-file\n", av[0]);
+		exit(1);
+	}
+	fd = open(av[1], O_RDWR);
+	if (fd < 0) {
+		perror(av[1]);
+		exit(1);
+	}
 
-     nnote = 12 + ROUNDUP(strlen(arch) + 1) + sizeof(descr);
-     nnote2 = 12 + ROUNDUP(strlen(rpaname) + 1) + sizeof(rpanote);
+	nnote = 12 + ROUNDUP(strlen(arch) + 1) + sizeof(descr);
+	nnote2 = 12 + ROUNDUP(strlen(rpaname) + 1) + sizeof(rpanote);
 
-     n = read(fd, buf, sizeof(buf));
-     if (n < 0) {
-	  perror("read");
-	  exit(1);
-     }
+	n = read(fd, buf, sizeof(buf));
+	if (n < 0) {
+		perror("read");
+		exit(1);
+	}
 
-     if (n < E_HSIZE || memcmp(&buf[E_IDENT+EI_MAGIC], elf_magic, 4) != 0)
-	  goto notelf;
+	if (n < E_HSIZE || memcmp(&buf[E_IDENT + EI_MAGIC], elf_magic, 4) != 0)
+		goto notelf;
 
-     if (buf[E_IDENT+EI_CLASS] != ELFCLASS32
-	 || buf[E_IDENT+EI_DATA] != ELFDATA2MSB) {
-	  fprintf(stderr, "%s is not a big-endian 32-bit ELF image\n",
-		  av[1]);
-	  exit(1);
-     }
+	if (buf[E_IDENT + EI_CLASS] != ELFCLASS32 || buf[E_IDENT + EI_DATA] != ELFDATA2MSB) {
+		fprintf(stderr, "%s is not a big-endian 32-bit ELF image\n", av[1]);
+		exit(1);
+	}
 
-     ph = GET_32BE(E_PHOFF);
-     ps = GET_16BE(E_PHENTSIZE);
-     np = GET_16BE(E_PHNUM);
-     if (ph < E_HSIZE || ps < PH_HSIZE || np < 1)
-	  goto notelf;
-     if (ph + (np + 2) * ps + nnote + nnote2 > n)
-	  goto nospace;
+	ph = GET_32BE(E_PHOFF);
+	ps = GET_16BE(E_PHENTSIZE);
+	np = GET_16BE(E_PHNUM);
+	if (ph < E_HSIZE || ps < PH_HSIZE || np < 1)
+		goto notelf;
+	if (ph + (np + 2) * ps + nnote + nnote2 > n)
+		goto nospace;
 
-     for (i = 0; i < np; ++i) {
-	  if (GET_32BE(ph + PH_TYPE) == PT_NOTE) {
-	       fprintf(stderr, "%s already has a note entry\n",
-		       av[1]);
-	       exit(0);
-	  }
-	  ph += ps;
-     }
+	for (i = 0; i < np; ++i) {
+		if (GET_32BE(ph + PH_TYPE) == PT_NOTE) {
+			fprintf(stderr, "%s already has a note entry\n", av[1]);
+			exit(0);
+		}
+		ph += ps;
+	}
 
-     /* XXX check that the area we want to use is all zeroes */
-     for (i = 0; i < 2 * ps + nnote + nnote2; ++i)
-	  if (buf[ph + i] != 0)
-	       goto nospace;
+	/* XXX check that the area we want to use is all zeroes */
+	for (i = 0; i < 2 * ps + nnote + nnote2; ++i)
+		if (buf[ph + i] != 0)
+			goto nospace;
 
-     /* fill in the program header entry */
-     ns = ph + 2 * ps;
-     PUT_32BE(ph + PH_TYPE, PT_NOTE);
-     PUT_32BE(ph + PH_OFFSET, ns);
-     PUT_32BE(ph + PH_FILESZ, nnote);
+	/* fill in the program header entry */
+	ns = ph + 2 * ps;
+	PUT_32BE(ph + PH_TYPE, PT_NOTE);
+	PUT_32BE(ph + PH_OFFSET, ns);
+	PUT_32BE(ph + PH_FILESZ, nnote);
 
-     /* fill in the note area we point to */
-     /* XXX we should probably make this a proper section */
-     PUT_32BE(ns, strlen(arch) + 1);
-     PUT_32BE(ns + 4, N_DESCR * 4);
-     PUT_32BE(ns + 8, 0x1275);
-     strcpy(&buf[ns + 12], arch);
-     ns += 12 + strlen(arch) + 1;
-     for (i = 0; i < N_DESCR; ++i, ns += 4)
-	  PUT_32BE(ns, descr[i]);
+	/* fill in the note area we point to */
+	/* XXX we should probably make this a proper section */
+	PUT_32BE(ns, strlen(arch) + 1);
+	PUT_32BE(ns + 4, N_DESCR * 4);
+	PUT_32BE(ns + 8, 0x1275);
+	strcpy(&buf[ns + 12], arch);
+	ns += 12 + strlen(arch) + 1;
+	for (i = 0; i < N_DESCR; ++i, ns += 4)
+		PUT_32BE(ns, descr[i]);
 
-     /* fill in the second program header entry and the RPA note area */
-     ph += ps;
-     PUT_32BE(ph + PH_TYPE, PT_NOTE);
-     PUT_32BE(ph + PH_OFFSET, ns);
-     PUT_32BE(ph + PH_FILESZ, nnote2);
+	/* fill in the second program header entry and the RPA note area */
+	ph += ps;
+	PUT_32BE(ph + PH_TYPE, PT_NOTE);
+	PUT_32BE(ph + PH_OFFSET, ns);
+	PUT_32BE(ph + PH_FILESZ, nnote2);
 
-     /* fill in the note area we point to */
-     PUT_32BE(ns, strlen(rpaname) + 1);
-     PUT_32BE(ns + 4, sizeof(rpanote));
-     PUT_32BE(ns + 8, 0x12759999);
-     strcpy(&buf[ns + 12], rpaname);
-     ns += 12 + ROUNDUP(strlen(rpaname) + 1);
-     for (i = 0; i < N_RPA_DESCR; ++i, ns += 4)
-	  PUT_32BE(ns, rpanote[i]);
+	/* fill in the note area we point to */
+	PUT_32BE(ns, strlen(rpaname) + 1);
+	PUT_32BE(ns + 4, sizeof(rpanote));
+	PUT_32BE(ns + 8, 0x12759999);
+	strcpy(&buf[ns + 12], rpaname);
+	ns += 12 + ROUNDUP(strlen(rpaname) + 1);
+	for (i = 0; i < N_RPA_DESCR; ++i, ns += 4)
+		PUT_32BE(ns, rpanote[i]);
 
-     /* Update the number of program headers */
-     PUT_16BE(E_PHNUM, np + 2);
+	/* Update the number of program headers */
+	PUT_16BE(E_PHNUM, np + 2);
 
-     /* write back */
-     lseek(fd, (long) 0, SEEK_SET);
-     i = write(fd, buf, n);
-     if (i < 0) {
-	  perror("write");
-	  exit(1);
-     }
-     if (i < n) {
-	  fprintf(stderr, "%s: write truncated\n", av[1]);
-	  exit(1);
-     }
+	/* write back */
+	lseek(fd, (long)0, SEEK_SET);
+	i = write(fd, buf, n);
+	if (i < 0) {
+		perror("write");
+		exit(1);
+	}
+	if (i < n) {
+		fprintf(stderr, "%s: write truncated\n", av[1]);
+		exit(1);
+	}
 
-     exit(0);
+	exit(0);
 
-notelf:
-     fprintf(stderr, "%s does not appear to be an ELF file\n", av[1]);
-     exit(1);
+      notelf:
+	fprintf(stderr, "%s does not appear to be an ELF file\n", av[1]);
+	exit(1);
 
-nospace:
-     fprintf(stderr, "sorry, I can't find space in %s to put the note\n",
-	     av[1]);
-     exit(1);
+      nospace:
+	fprintf(stderr, "sorry, I can't find space in %s to put the note\n", av[1]);
+	exit(1);
 }
 
 /* 
